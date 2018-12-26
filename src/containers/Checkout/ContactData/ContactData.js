4295d1f75aa1bf99828bd7e5a73140e5e364bc85
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-12-26 23:14:35 +0200

    Adding generic Input component, connecting the form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,16 +4,69 @@ import axios from "../../../axios-orders";
 import classes from "./ContactData.module.css";
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
-    name: "",
-    email: "",
-    mobile: "",
-    address: {
-      street: "",
-      zip: "",
-      city: ""
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name"
+        },
+        value: ""
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your E-Mail"
+        },
+        value: ""
+      },
+      mobile: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Mobile Number"
+        },
+        value: ""
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Street"
+        },
+        value: ""
+      },
+      zip: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "ZIP Code"
+        },
+        value: ""
+      },
+      city: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "City"
+        },
+        value: ""
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" }
+          ]
+        },
+        value: ""
+      }
     },
     loading: false
   };
@@ -24,16 +77,16 @@ class ContactData extends Component {
       ingredients: this.props.ingredients,
       price: this.props.price,
       customer: {
-        name: this.state.name,
+        name: this.state.orderForm.name.value,
         address: {
-          street: this.state.street,
-          zip: this.state.zip,
-          city: this.state.city
+          street: this.state.orderForm.street.value,
+          zip: this.state.orderForm.zip.value,
+          city: this.state.orderForm.city.value
         },
-        email: this.state.email,
-        mobile: this.state.mobile
+        email: this.state.orderForm.email.value,
+        mobile: this.state.orderForm.mobile.value
       },
-      deliveryMethod: "fastest"
+      deliveryMethod: this.state.orderForm.deliveryMethod.value
     };
     this.setState({ loading: true });
     axios
@@ -45,45 +98,38 @@ class ContactData extends Component {
       .catch(err => this.setState({ loading: false }));
   };
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({ orderForm: updatedOrderForm });
+  };
+
   render() {
+    const formElementArray = [];
+    for (let key in this.state.orderForm) {
+      formElementArray.push({
+        id: key,
+        settings: this.state.orderForm[key]
+      });
+    }
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your Name"
-        />
-        <input
-          className={classes.Input}
-          type="email"
-          name="email"
-          placeholder="Your Mail"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="mobile"
-          placeholder="Your Mobile Number"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Street"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="zip"
-          placeholder="Zip"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="city"
-          placeholder="City"
-        />
+        {formElementArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.settings.elementType}
+            elementConfig={formElement.settings.elementConfig}
+            value={formElement.settings.value}
+            changed={event => this.inputChangedHandler(event, formElement.id)}
+          />
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>
           Order
         </Button>
