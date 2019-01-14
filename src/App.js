49c7465a29f9d1c885ff6ceaba8d6290b74e77da
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2019-01-14 23:43:48 +0200

    Lazy loading Checkout, Orders & Auth routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import { Route, withRouter, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
+
+const asyncCheckout = asyncComponent(() => {
+  return import("./containers/Checkout/Checkout");
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import("./containers/Orders/Orders");
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import("./containers/Auth/Auth");
+});
 
 class App extends Component {
   componentDidMount() {
@@ -19,7 +29,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route exact path="/" component={BurgerBuilder} />
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Redirect to="/" />
       </Switch>
     );
@@ -28,10 +38,10 @@ class App extends Component {
       routes = (
         <Switch>
           <Route exact path="/" component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Redirect to="/" />
         </Switch>
       );
